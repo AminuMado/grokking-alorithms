ae5529cb83154925d201968d269ff1e9d700f411
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-04-07 09:24:40 +0100

    merge question solution added

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-//Insertion Sort
-// This is another sorting algorithm that has an alternative approach to sorting from bubble sort and selection sort
-// Steps
-// As with most sorting algorithms you start with a loop to iterate through all the array
-// you select i+1 in the first pass-through (the second cell).
-// you make a comparision with the cell preceding it, if the element preceding it is greater you swap them around else you move on to the next pass
-// and you loop to the next element
-//
-
-const insertionSort = (array) => {
-  // for index in range(1, len(array)):
-  // temp_value = array[index]
-  // position = index - 1
-  // while position >= 0:
-  // if array[position] > temp_value:
-  // array[position + 1] = array[position]
-  // position = position - 1
-
-  for (let i = 0; i < array.length - 1; i++) {
-    const tempValue = array[i + 1];
-    let position = i;
-
-    while (position >= 0) {
-      if (array[position] > tempValue) {
-        //make the shift of position
-        array[position + 1] = array[position];
-      } else break;
-
-      position--;
+const merge = (array_1, array_2) => {
+  const new_array = [];
+  let array_1_pointer = 0;
+  let array_2_pointer = 0;
+  let count = 0;
+  // Run the loop until we've reached end of both arrays:
+  while (array_1_pointer < array_1.length || array_2_pointer < array_2.length) {
+    count++;
+    // If we already reached the end of the first array,
+    //  add item from second array:
+    if (!array_1[array_1_pointer]) {
+      new_array.push(array_2[array_2_pointer]);
+      array_2_pointer += 1;
     }
-    array[position + 1] = tempValue;
+    // If we already reached the end of the second array,
+    // add item from first array:
+    else if (!array_2[array_2_pointer]) {
+      new_array.push(array_1[array_1_pointer]);
+      array_1_pointer += 1;
+    }
+    // If the current number in first array is less than current
+    //  number in second array, add from first array:
+    else if (array_1[array_1_pointer] < array_2[array_2_pointer]) {
+      new_array.push(array_1[array_1_pointer]);
+      array_1_pointer += 1;
+    }
+    //  If the current number in second array is less than or equal
+    // to current number in first array, add from second array:
+    else {
+      new_array.push(array_2[array_2_pointer]);
+      array_2_pointer += 1;
+    }
   }
-  return array;
+  console.log(count);
+  return new_array;
 };
-
-const myArray = [10, 9, 8, 7, 5, 1, 3, 2, 4, 6];
-const sortedArray = insertionSort(myArray);
-console.log(sortedArray); //[1,2,3,4,5,6,7,8,9,10]
+const arr1 = [1, 2, 3, 4];
+const arr2 = [2, 3, 4, 5, 6, 7, 8];
+const ans = merge(arr1, arr2);
+console.log(ans);
